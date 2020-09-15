0d7a11dffbb7c3ffeb5a866bfc2a328a2ead22ef
Fares <Fares.steen@gmail.com>
2020-09-15 19:32:37 +0200

    finish camera zoom

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -75,7 +75,54 @@ public class CameraControl : MonoBehaviour
 
     private void ZoomTheCamera()
     {
-
+        float requiredSize = FindRequiredSize();
+        SetCameraOrthographicSize(requiredSize);
     }
 
+    private void SetCameraOrthographicSize(float RequiredSize)
+    {
+        camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, RequiredSize, ref zoomSpeed, dampTime);
+    }
+
+    private float FindRequiredSize()
+    {
+        Vector3 desiredLocalPosition = transform.InverseTransformPoint(desiredPosition);
+
+        float size = 0f;
+
+        for (int i = 0; i < targetTanks.Length; i++)
+        {
+            if (!IsTheTankStillActive(targetTanks[i]))
+                continue;
+
+            Vector3 targetLocalPosition = FindTankTargetInLocalPositionInCameraReg(targetTanks[i].position);
+
+            Vector3 desiredPositionToTarget = targetLocalPosition - desiredLocalPosition;
+
+            size = Mathf.Max(size, Mathf.Abs(desiredPositionToTarget.y));
+
+            size = Mathf.Max(size, Mathf.Abs(desiredPositionToTarget.x) / camera.aspect);
+
+        }
+
+
+        size += screenEdgeBuffer;
+
+        //to prevent size to be less that min
+        size = Mathf.Max(size, minSize);
+
+        return size;
+    }
+
+    private Vector3 FindTankTargetInLocalPositionInCameraReg(Vector3 TankPosition)
+    {
+        return transform.InverseTransformPoint(TankPosition);
+    }
+
+    public void SetStartPositionAndSize()
+    {
+        FindAveragePosition();
+        transform.position = desiredPosition;
+        camera.orthographicSize = FindRequiredSize();
+    }
 }
