62a0383cb6d8df0a65983cec0ad44eb54406909e
Fares <Fares.steen@gmail.com>
2020-09-17 16:50:20 +0200

    Finish tank shooting

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -33,15 +33,106 @@ public class TankShooting : MonoBehaviour
     [SerializeField]
     private float maxChargeTime = 0.75f;
 
+    private string fireButton;
+    private float currentLaunchForce;
+    private float chargeSpeed;
+    private bool fired;
 
-    void Start()
+
+    private void OnEnable()
     {
+        currentLaunchForce = minLaunchForce;
+        aimSlider.value = minLaunchForce;
+    }
+    private void Start()
+    {
+        fireButton = "Fire" + playerNumber;
+        chargeSpeed = (maxLaunchForce - minLaunchForce) / maxChargeTime;
+    }
+
+
+    private void Update()
+    {
+        aimSlider.value = minLaunchForce;
+
+        if (MaxChargedAndNotFired())
+        {
+            currentLaunchForce = maxLaunchForce;
+            Fire();
+        }
+        else if (FireButtonPressedForFirestTime())
+        {
+            ChargingBegins();
+        }
+        else if (HoldingTheFireButtonAndNotFired())
+        {
+            ExtendeTheLaunchForceAndTheSlider();
+        }
+        else if (FireButtonReleasedAndNotFired())
+        {
+            Fire();
+        }
+    }
+
+    private bool FireButtonReleasedAndNotFired()
+    {
+        return Input.GetButtonUp(fireButton) && !fired;
+    }
+
+    private void ExtendeTheLaunchForceAndTheSlider()
+    {
+        currentLaunchForce += chargeSpeed * Time.deltaTime;
+        aimSlider.value = currentLaunchForce;
+    }
+
+    private void ChargingBegins()
+    {
+        fired = false;
+        currentLaunchForce = minLaunchForce;
+        shootingAudioSource.clip = ChargingClip;
+        shootingAudioSource.Play();
+    }
+
+    private bool HoldingTheFireButtonAndNotFired()
+    {
+        return Input.GetButton(fireButton) && !fired;
+    }
+
+    private bool FireButtonPressedForFirestTime()
+    {
+        return Input.GetButtonDown(fireButton);
+    }
+
+    private bool MaxChargedAndNotFired()
+    {
+        return currentLaunchForce >= maxLaunchForce && !fired;
+    }
+
+    private void Fire()
+    {
+        fired = true;
+        Rigidbody shellInstance = CreateTheShell();
+        FireTheShell(shellInstance);
+
+        PlayShootingSound();
+
+        currentLaunchForce = minLaunchForce;
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PlayShootingSound()
     {
+        shootingAudioSource.clip = fireClip;
+        shootingAudioSource.Play();
+    }
 
+    private void FireTheShell(Rigidbody shellInstance)
+    {
+        shellInstance.velocity = currentLaunchForce * fireTransform.forward;
+    }
+
+    private Rigidbody CreateTheShell()
+    {
+        return Instantiate(shell, fireTransform.position, fireTransform.rotation) as Rigidbody;
     }
 }
