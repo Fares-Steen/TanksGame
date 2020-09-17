4dd8c58899cd985862ee25968978b5962a8b8c38
Fares <Fares.steen@gmail.com>
2020-09-17 15:45:14 +0200

    change to text

---
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankShooting : MonoBehaviour
+{
+    [SerializeField]
+    private int playerNumber = 1;
+
+    [SerializeField]
+    private Rigidbody shell;
+
+    [SerializeField]
+    private Transform fireTransform;
+
+    [SerializeField]
+    private Slider aimSlider;
+
+    [SerializeField]
+    private AudioSource shootingAudioSource;
+
+    [SerializeField]
+    private AudioClip ChargingClip;
+
+    [SerializeField]
+    private AudioClip fireClip;
+
+    [SerializeField]
+    private float minLaunchForce = 15f;
+
+    [SerializeField]
+    private float maxLaunchForce = 30f;
+
+    [SerializeField]
+    private float maxChargeTime = 0.75f;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
